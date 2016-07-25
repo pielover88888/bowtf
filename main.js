46dbed3d4ecaca8c03192cab03171cbae4065370
Jeff <pielover88888@live.com>
2016-07-25 15:01:24 -0400

    Add main.js and a triangle

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+	// Create the canvas
+	createCanvas(720, 400);
+	background(200);
+  
+	// Set colors
+	fill(204, 101, 192, 127);
+	stroke(127, 63, 120);
+	triangle(300, 100, 320, 100, 310, 80);
+}
