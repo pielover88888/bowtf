9955652686dc2baae5bc293eca69995124da845a
Jeff <pielover88888@live.com>
2016-07-25 17:00:07 -0400

    Proper rotation

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function setup() {
 	// Create the canvas
 	createCanvas(720, 400);
 }
-x = 150;
+x = 100;
 y = 100;
 function draw() {
 	clear();
 	background(130);
 	translate(x,y);
-	rotate(10);
-	image(img,0,0);
+	rotate(random(0,PI*2));
+	image(img,0-img.width/2,0-img.height/2,img.width/2,img.height/2);
 	translate(-x,-y);
 }
