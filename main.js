576e5bfc4e7032050cb52bfb1b2f04e440e50660
Godwhoa <gjdaniel1999@gmail.com>
2016-07-26 01:00:51 +0530

    Draw sprite

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+var img;
+function preload() {
+  img = loadImage("assets/triangle_white.png");
+}
+
 function setup() {
 	// Create the canvas
 	createCanvas(720, 400);
 	background(200);
-	// Set colors
-	fill(204, 101, 192, 127);
-	stroke(127, 63, 120);
-	triangle(300, 100, 320, 100, 310, 80);
+}
+
+function draw() {
+  image(img, 0, 0);
 }
