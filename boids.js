866536e96d62ecad720bfc9e30bb754ad1d74d2a
Godwhoa <gjdaniel1999@gmail.com>
2016-07-26 07:39:23 +0530

    Add velocity and tele.

--- boids.js
+++ boids.js
@@ -9,19 +9,40 @@ var Boids = function(count) {
 /* Generates the flock */
 Boids.prototype.gen = function() {
 	for (var i = 0; i < this.count; i++) {
-		this.points[i] = {"x":random(width),"y":random(height),"angle":random(TWO_PI)}
+		this.points[i] = {
+			"x":random(width),"y":random(height),
+			"vx":0,"vy":4,
+			"angle":random(TWO_PI),
+		}
 	}
 }
 
 /* Draws the flock */
 Boids.prototype.draw = function() {
 	for (var i = 0; i < this.points.length; i++) {
-		var point = this.points[i]
+		/* Intergrate velocity */
+		this.points[i].x += this.points[i].vx
+		this.points[i].y += this.points[i].vy
+		console.log(this.points[i].vy)
+		/* Teleport */
+		if (this.points[i].x < 0) {
+			this.points[i].x = width
+		}
+		if (this.points[i].x > width) {
+			this.points[i].x = 0
+		}
+		if (this.points[i].y < 0) {
+			this.points[i].y = height
+		}
+		if (this.points[i].y > height) {
+			this.points[i].y = 0
+		}
+		/* Draw */ 
 		push()
-		translate(point.x,point.y)
+		translate(this.points[i].x, this.points[i].y)
 		rotate(point.angle)
 		image(this.img,0-this.img.width/4,0-this.img.height/4,this.img.width/2,this.img.height/2)
-		translate(-point.x,-point.y)
+		translate(-this.points[i].x, -this.points[i].y)
 		pop()
 	}
 }
